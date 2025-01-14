d424b30eeec844247610da4c932c4b7a8807a85f
tangxiangong <tangxiangong@gmail.com>
2025-01-14 09:59:51 +0800

    feat: 添加配置模块以管理数据库连接和 TCP 监听器

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,36 +1,20 @@
-use sqlx::mysql::MySqlPoolOptions;
-use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
 use tracing::info;
 
+#[path = "config.rs"]
+mod config;
+
 #[tokio::main]
 async fn main() {
-    dotenvy::dotenv().ok();
+    let (pool, listener) = config::config().await;
+
     tracing_subscriber::fmt::init();
-
-    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is not set");
-
-    let pool = MySqlPoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url)
-        .await
-        .expect("Failed to connect to MySQL");
-
-    // migrate!("../migrations")
-    //     .run(&pool)
-    //     .await
-    //     .expect("Failed to migrate database");
-
     let app = api::compose()
         .layer(TraceLayer::new_for_http())
         .with_state(pool);
-
-    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
-
     info!(
         "server listener on http://{}",
         listener.local_addr().unwrap()
     );
-
     axum::serve(listener, app).await.unwrap();
 }
